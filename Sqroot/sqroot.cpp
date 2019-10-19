247f66d65f106e1dfe1ef85d776edb11c67c9bbe
Ulelu Israel <Uleluisrael@gmail.com>
2019-10-19 20:42:21 +0100

    Converted Square root code from java to C++

---
+++ Sqroot/sqroot.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+int main(){
+	
+cout << "Program to calculate the square root of a number\nEnter a number: ";
+
+double num;
+cin >> num;
+double result = sqrt (num);
+cout << result;
+	return 0;
+}
